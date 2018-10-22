e01505e3aece80da52e91498edcd42a7b8f88469
Vincent Fiduccia <vincent@rancher.com>
2018-10-22 14:00:28 -0700

    Allow forwarding github auth to other localhost ports

--- app/verify-auth/route.js
+++ app/verify-auth/route.js
@@ -3,14 +3,42 @@ import RSVP from 'rsvp';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import C from 'shared/utils/constants';
+import { addQueryParams, parseUrl } from 'shared/utils/util';
+import { reject } from 'rsvp';
 import VerifyAuth from 'ui/mixins/verify-auth';
 
 const samlProviders = ['ping', 'adfs', 'keycloak'];
+const allowedForwards = ['localhost'];
 
 export default Route.extend(VerifyAuth, {
   github: service(),
 
   model(params/* , transition */) {
+    const github  = get(this, 'github');
+    const code    = get(params, 'code');
+    const forward = get(params, 'forward');
+
+    // Allow another redirect if the hostname is in the whitelist above.
+    // This allows things like sharing github auth between rancher at localhost:8000
+    // and rio dev at localhost:8004
+    if ( forward ) {
+      const parsed = parseUrl(forward);
+      if ( allowedForwards.includes(parsed.hostname.toLowerCase()) ) {
+        if ( get(params, 'login') ) {
+          window.location.href = addQueryParams(forward, {
+            forwarded: 'true',
+            code
+          });
+        } else {
+          github.login(forward);
+        }
+      } else {
+        return reject(new Error('Invalid forward url'));
+      }
+
+      return;
+    }
+
     if (window.opener && !get(params, 'login')) {
       let openersGithub = window.opener.ls('github');
       let openerStore   = window.opener.ls('globalStore');
@@ -44,12 +72,12 @@ export default Route.extend(VerifyAuth, {
       }
     }
 
-    if (get(params, 'code') && get(params, 'login')) {
-      if (get(this, 'github').stateMatches(get(params, 'state'))) {
+    if ( code && get(params, 'login')) {
+      if ( github.stateMatches(get(params, 'state')) ) {
         let ghProvider = get(this, 'access.providers').findBy('id', 'github');
 
         return ghProvider.doAction('login', {
-          code:         get(params, 'code'),
+          code,
           responseType: 'cookie',
           description:  C.SESSION.DESCRIPTION,
           ttl:          C.SESSION.TTL,
