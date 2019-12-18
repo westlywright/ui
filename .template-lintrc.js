8b841a434e10877dc4863de20596cf24f03e47da
Westly Wright <westly.wright@gmail.com>
2019-12-18 12:18:23 -0700

    Turn on no-bare-strings template lint rule and fix errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -11,7 +11,74 @@ module.exports = {
     'linebreak-style':            false,
     'link-rel-noopener':          false,
     'no-attrs-in-components':     false,
-    'no-bare-strings':            false,
+    'no-bare-strings':            [
+      '&lpar;', // (
+      '&rpar;', // )
+      '&comma;', // ,
+      '&period;', // .
+      '&amp;', // &
+      '&AMP;', // &
+      '&plus;', // +
+      '&minus;', // -
+      '&equals;', // =
+      '&ast;', // *
+      '&midast;', // *
+      '&sol;', // /
+      '&num;', // #
+      '&percnt;', // %
+      '&excl;', // !
+      '&quest;', // ?
+      '&colon;', // :
+      '&lsqb;', // [
+      '&lbrack;', // [
+      '&rsqb;', // ]
+      '&rbrack;', // ]
+      '&lcub;', // {
+      '&lbrace;', // {
+      '&rcub;', // }
+      '&rbrace;', // }
+      '&lt;', // <
+      '&LT;', // <
+      '&gt;', // >
+      '&GT;', // >
+      '&bull;', // •
+      '&bullet;', // •
+      '&mdash;', // —
+      '&nbsp;', // non-breaking space
+      '&Tab;',
+      '&NewLine;',
+      '&verbar;', // |
+      '&vert;', // |
+      '&VerticalLine;', // |
+      '&ndash;', // -
+      '(',
+      ')',
+      ',',
+      '.',
+      '&',
+      '+',
+      '-',
+      '=',
+      '*',
+      '/',
+      '#',
+      '%',
+      '!',
+      '?',
+      ':',
+      '[',
+      ']',
+      '{',
+      '}',
+      '<',
+      '>',
+      '•',
+      '—',
+      ' ',
+      '|',
+      '~',
+      '"',
+    ],
     'no-debugger':                false,
     'no-duplicate-attributes':    false,
     'no-html-comments':           false,
